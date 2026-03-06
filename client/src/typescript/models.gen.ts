aaaca7888bfc9c88e6e885150b4c0fbdc875c29d
jinius <0xjinius@gmail.com>
2026-03-06 01:42:21 -0800

    deploy

--- client/src/typescript/models.gen.ts
+++ client/src/typescript/models.gen.ts
@@ -4,20 +4,20 @@ import { CairoCustomEnum, CairoOption, CairoOptionVariant, BigNumberish } from '
 
 // Type definition for `hexed::models::GameCounter` struct
 export interface GameCounter {
-	game_id: BigNumberish;
+	token_id: BigNumberish;
 	active_games: BigNumberish;
 }
 
 // Type definition for `hexed::models::GameSession` struct
 export interface GameSession {
-	game_id: BigNumberish;
+	token_id: BigNumberish;
 	player: string;
 	is_active: boolean;
 }
 
 // Type definition for `hexed::models::HighestScore` struct
 export interface HighestScore {
-	game_id: BigNumberish;
+	token_id: BigNumberish;
 	player: string;
 	username: BigNumberish;
 	xp: BigNumberish;
@@ -25,7 +25,7 @@ export interface HighestScore {
 
 // Type definition for `hexed::models::PlayerState` struct
 export interface PlayerState {
-	game_id: BigNumberish;
+	token_id: BigNumberish;
 	position: Vec2;
 	last_direction: CairoOption<DirectionEnum>;
 	can_move: boolean;
@@ -33,7 +33,7 @@ export interface PlayerState {
 
 // Type definition for `hexed::models::PlayerStats` struct
 export interface PlayerStats {
-	game_id: BigNumberish;
+	token_id: BigNumberish;
 	hp: BigNumberish;
 	max_hp: BigNumberish;
 	xp: BigNumberish;
@@ -43,7 +43,7 @@ export interface PlayerStats {
 export interface TileOccupant {
 	x: BigNumberish;
 	y: BigNumberish;
-	game_id: BigNumberish;
+	token_id: BigNumberish;
 }
 
 // Type definition for `hexed::models::Vec2` struct
@@ -54,8 +54,8 @@ export interface Vec2 {
 
 // Type definition for `hexed::systems::game::contracts::game_systems::CombatResult` struct
 export interface CombatResult {
-	attacker_game_id: BigNumberish;
-	defender_game_id: BigNumberish;
+	attacker_token_id: BigNumberish;
+	defender_token_id: BigNumberish;
 	attacker_won: boolean;
 	attacker_position: Vec2;
 	defender_position: Vec2;
@@ -69,7 +69,7 @@ export interface CombatResult {
 
 // Type definition for `hexed::systems::game::contracts::game_systems::EncounterOccurred` struct
 export interface EncounterOccurred {
-	game_id: BigNumberish;
+	token_id: BigNumberish;
 	is_gift: boolean;
 	outcome: BigNumberish;
 	hp_after: BigNumberish;
@@ -87,35 +87,67 @@ export interface HighestScoreUpdated {
 
 // Type definition for `hexed::systems::game::contracts::game_systems::Moved` struct
 export interface Moved {
-	game_id: BigNumberish;
+	token_id: BigNumberish;
 	direction: DirectionEnum;
 	position: Vec2;
 }
 
 // Type definition for `hexed::systems::game::contracts::game_systems::NeighborsRevealed` struct
 export interface NeighborsRevealed {
-	game_id: BigNumberish;
+	token_id: BigNumberish;
 	position: Vec2;
 	neighbors: BigNumberish;
 }
 
 // Type definition for `hexed::systems::game::contracts::game_systems::PlayerDied` struct
 export interface PlayerDied {
-	game_id: BigNumberish;
+	token_id: BigNumberish;
 	killed_by: BigNumberish;
 	position: Vec2;
 }
 
 // Type definition for `hexed::systems::game::contracts::game_systems::Spawned` struct
 export interface Spawned {
-	game_id: BigNumberish;
+	token_id: BigNumberish;
 	player: string;
 	position: Vec2;
 }
 
+// Type definition for `game_components_interfaces::structs::metagame::GameContext` struct
+export interface GameContext {
+	name: BigNumberish;
+	value: BigNumberish;
+}
+
+// Type definition for `game_components_interfaces::structs::metagame::GameContextDetails` struct
+export interface GameContextDetails {
+	name: string;
+	description: string;
+	id: CairoOption<BigNumberish>;
+	context: Array<GameContext>;
+}
+
+// Type definition for `game_components_interfaces::structs::minigame::MintGameParams` struct
+export interface MintGameParams {
+	player_name: CairoOption<BigNumberish>;
+	settings_id: CairoOption<BigNumberish>;
+	start: CairoOption<BigNumberish>;
+	end: CairoOption<BigNumberish>;
+	objective_id: CairoOption<BigNumberish>;
+	context: CairoOption<GameContextDetails>;
+	client_url: CairoOption<string>;
+	renderer_address: CairoOption<string>;
+	skills_address: CairoOption<string>;
+	to: string;
+	soulbound: boolean;
+	paymaster: boolean;
+	salt: BigNumberish;
+	metadata: BigNumberish;
+}
+
 // Type definition for `hexed::models::GameState` struct
 export interface GameState {
-	game_id: BigNumberish;
+	token_id: BigNumberish;
 	player: string;
 	position: Vec2;
 	last_direction: CairoOption<DirectionEnum>;
@@ -155,34 +187,37 @@ export interface SchemaType extends ISchemaType {
 		NeighborsRevealed: NeighborsRevealed,
 		PlayerDied: PlayerDied,
 		Spawned: Spawned,
+		GameContext: GameContext,
+		GameContextDetails: GameContextDetails,
+		MintGameParams: MintGameParams,
 		GameState: GameState,
 	},
 }
 export const schema: SchemaType = {
 	hexed: {
 		GameCounter: {
-			game_id: 0,
+			token_id: 0,
 			active_games: 0,
 		},
 		GameSession: {
-			game_id: 0,
+			token_id: 0,
 			player: "",
 			is_active: false,
 		},
 		HighestScore: {
-			game_id: 0,
+			token_id: 0,
 			player: "",
 			username: 0,
 			xp: 0,
 		},
 		PlayerState: {
-			game_id: 0,
+			token_id: 0,
 		position: { x: 0, y: 0, },
 			last_direction: new CairoOption(CairoOptionVariant.None),
 			can_move: false,
 		},
 		PlayerStats: {
-			game_id: 0,
+			token_id: 0,
 			hp: 0,
 			max_hp: 0,
 			xp: 0,
@@ -190,15 +225,15 @@ export const schema: SchemaType = {
 		TileOccupant: {
 			x: 0,
 			y: 0,
-			game_id: 0,
+			token_id: 0,
 		},
 		Vec2: {
 			x: 0,
 			y: 0,
 		},
 		CombatResult: {
-			attacker_game_id: 0,
-			defender_game_id: 0,
+			attacker_token_id: 0,
+			defender_token_id: 0,
 			attacker_won: false,
 		attacker_position: { x: 0, y: 0, },
 		defender_position: { x: 0, y: 0, },
@@ -210,7 +245,7 @@ export const schema: SchemaType = {
 			defender_died: false,
 		},
 		EncounterOccurred: {
-			game_id: 0,
+			token_id: 0,
 			is_gift: false,
 			outcome: 0,
 			hp_after: 0,
@@ -224,7 +259,7 @@ export const schema: SchemaType = {
 			xp: 0,
 		},
 		Moved: {
-			game_id: 0,
+			token_id: 0,
 		direction: new CairoCustomEnum({ 
 					East: "",
 				NorthEast: undefined,
@@ -235,22 +270,48 @@ export const schema: SchemaType = {
 		position: { x: 0, y: 0, },
 		},
 		NeighborsRevealed: {
-			game_id: 0,
+			token_id: 0,
 		position: { x: 0, y: 0, },
 			neighbors: 0,
 		},
 		PlayerDied: {
-			game_id: 0,
+			token_id: 0,
 			killed_by: 0,
 		position: { x: 0, y: 0, },
 		},
 		Spawned: {
-			game_id: 0,
+			token_id: 0,
 			player: "",
 		position: { x: 0, y: 0, },
 		},
+		GameContext: {
+			name: 0,
+			value: 0,
+		},
+		GameContextDetails: {
+		name: "",
+		description: "",
+			id: new CairoOption(CairoOptionVariant.None),
+			context: [{ name: 0, value: 0, }],
+		},
+		MintGameParams: {
+			player_name: new CairoOption(CairoOptionVariant.None),
+			settings_id: new CairoOption(CairoOptionVariant.None),
+			start: new CairoOption(CairoOptionVariant.None),
+			end: new CairoOption(CairoOptionVariant.None),
+			objective_id: new CairoOption(CairoOptionVariant.None),
+			context: new CairoOption(CairoOptionVariant.None),
+			client_url: new CairoOption(CairoOptionVariant.None),
+			renderer_address: new CairoOption(CairoOptionVariant.None),
+			skills_address: new CairoOption(CairoOptionVariant.None),
+			to: "",
+			soulbound: false,
+			paymaster: false,
+			salt: 0,
+			metadata: 0,
+		},
 		GameState: {
-			game_id: 0,
+			token_id: 0,
 			player: "",
 		position: { x: 0, y: 0, },
 			last_direction: new CairoOption(CairoOptionVariant.None),
@@ -279,5 +340,8 @@ export enum ModelsMapping {
 	NeighborsRevealed = 'hexed-NeighborsRevealed',
 	PlayerDied = 'hexed-PlayerDied',
 	Spawned = 'hexed-Spawned',
+	GameContext = 'game_components_interfaces-GameContext',
+	GameContextDetails = 'game_components_interfaces-GameContextDetails',
+	MintGameParams = 'game_components_interfaces-MintGameParams',
 	GameState = 'hexed-GameState',
 }
--- contracts/bindings/typescript/contracts.gen.ts
+++ contracts/bindings/typescript/contracts.gen.ts
@@ -4,17 +4,17 @@ import * as models from "./models.gen";
 
 export function setupWorld(provider: DojoProvider) {
 
-	const build_game_systems_getGameState_calldata = (gameId: BigNumberish): DojoCall => {
+	const build_game_systems_getGameState_calldata = (tokenId: BigNumberish): DojoCall => {
 		return {
 			contractName: "game_systems",
 			entrypoint: "get_game_state",
-			calldata: [gameId],
+			calldata: [tokenId],
 		};
 	};
 
-	const game_systems_getGameState = async (gameId: BigNumberish) => {
+	const game_systems_getGameState = async (tokenId: BigNumberish) => {
 		try {
-			return await provider.call("hexed", build_game_systems_getGameState_calldata(gameId));
+			return await provider.call("hexed", build_game_systems_getGameState_calldata(tokenId));
 		} catch (error) {
 			console.error(error);
 			throw error;
@@ -38,19 +38,19 @@ export function setupWorld(provider: DojoProvider) {
 		}
 	};
 
-	const build_game_systems_move_calldata = (gameId: BigNumberish, direction: CairoCustomEnum): DojoCall => {
+	const build_game_systems_move_calldata = (tokenId: BigNumberish, direction: CairoCustomEnum): DojoCall => {
 		return {
 			contractName: "game_systems",
 			entrypoint: "move",
-			calldata: [gameId, direction],
+			calldata: [tokenId, direction],
 		};
 	};
 
-	const game_systems_move = async (snAccount: Account | AccountInterface, gameId: BigNumberish, direction: CairoCustomEnum) => {
+	const game_systems_move = async (snAccount: Account | AccountInterface, tokenId: BigNumberish, direction: CairoCustomEnum) => {
 		try {
 			return await provider.execute(
 				snAccount,
-				build_game_systems_move_calldata(gameId, direction),
+				build_game_systems_move_calldata(tokenId, direction),
 				"hexed",
 			);
 		} catch (error) {
@@ -80,21 +80,191 @@ export function setupWorld(provider: DojoProvider) {
 		}
 	};
 
-	const build_game_systems_spawn_calldata = (): DojoCall => {
+	const build_game_systems_spawn_calldata = (tokenId: BigNumberish): DojoCall => {
 		return {
 			contractName: "game_systems",
 			entrypoint: "spawn",
+			calldata: [tokenId],
+		};
+	};
+
+	const game_systems_spawn = async (snAccount: Account | AccountInterface, tokenId: BigNumberish) => {
+		try {
+			return await provider.execute(
+				snAccount,
+				build_game_systems_spawn_calldata(tokenId),
+				"hexed",
+			);
+		} catch (error) {
+			console.error(error);
+			throw error;
+		}
+	};
+
+	const build_game_token_systems_gameOver_calldata = (tokenId: BigNumberish): DojoCall => {
+		return {
+			contractName: "game_token_systems",
+			entrypoint: "game_over",
+			calldata: [tokenId],
+		};
+	};
+
+	const game_token_systems_gameOver = async (tokenId: BigNumberish) => {
+		try {
+			return await provider.call("hexed", build_game_token_systems_gameOver_calldata(tokenId));
+		} catch (error) {
+			console.error(error);
+			throw error;
+		}
+	};
+
+	const build_game_token_systems_gameOverBatch_calldata = (tokenIds: Array<BigNumberish>): DojoCall => {
+		return {
+			contractName: "game_token_systems",
+			entrypoint: "game_over_batch",
+			calldata: [tokenIds],
+		};
+	};
+
+	const game_token_systems_gameOverBatch = async (tokenIds: Array<BigNumberish>) => {
+		try {
+			return await provider.call("hexed", build_game_token_systems_gameOverBatch_calldata(tokenIds));
+		} catch (error) {
+			console.error(error);
+			throw error;
+		}
+	};
+
+	const build_game_token_systems_mintGame_calldata = (playerName: CairoOption<BigNumberish>, settingsId: CairoOption<BigNumberish>, start: CairoOption<BigNumberish>, end: CairoOption<BigNumberish>, objectiveId: CairoOption<BigNumberish>, context: CairoOption<GameContextDetails>, clientUrl: CairoOption<string>, rendererAddress: CairoOption<string>, skillsAddress: CairoOption<string>, to: string, soulbound: boolean, paymaster: boolean, salt: BigNumberish, metadata: BigNumberish): DojoCall => {
+		return {
+			contractName: "game_token_systems",
+			entrypoint: "mint_game",
+			calldata: [playerName, settingsId, start, end, objectiveId, context, clientUrl, rendererAddress, skillsAddress, to, soulbound, paymaster, salt, metadata],
+		};
+	};
+
+	const game_token_systems_mintGame = async (playerName: CairoOption<BigNumberish>, settingsId: CairoOption<BigNumberish>, start: CairoOption<BigNumberish>, end: CairoOption<BigNumberish>, objectiveId: CairoOption<BigNumberish>, context: CairoOption<GameContextDetails>, clientUrl: CairoOption<string>, rendererAddress: CairoOption<string>, skillsAddress: CairoOption<string>, to: string, soulbound: boolean, paymaster: boolean, salt: BigNumberish, metadata: BigNumberish) => {
+		try {
+			return await provider.call("hexed", build_game_token_systems_mintGame_calldata(playerName, settingsId, start, end, objectiveId, context, clientUrl, rendererAddress, skillsAddress, to, soulbound, paymaster, salt, metadata));
+		} catch (error) {
+			console.error(error);
+			throw error;
+		}
+	};
+
+	const build_game_token_systems_mintGameBatch_calldata = (mints: Array<MintGameParams>): DojoCall => {
+		return {
+			contractName: "game_token_systems",
+			entrypoint: "mint_game_batch",
+			calldata: [mints],
+		};
+	};
+
+	const game_token_systems_mintGameBatch = async (mints: Array<MintGameParams>) => {
+		try {
+			return await provider.call("hexed", build_game_token_systems_mintGameBatch_calldata(mints));
+		} catch (error) {
+			console.error(error);
+			throw error;
+		}
+	};
+
+	const build_game_token_systems_objectivesAddress_calldata = (): DojoCall => {
+		return {
+			contractName: "game_token_systems",
+			entrypoint: "objectives_address",
 			calldata: [],
 		};
 	};
 
-	const game_systems_spawn = async (snAccount: Account | AccountInterface) => {
+	const game_token_systems_objectivesAddress = async () => {
 		try {
-			return await provider.execute(
-				snAccount,
-				build_game_systems_spawn_calldata(),
-				"hexed",
-			);
+			return await provider.call("hexed", build_game_token_systems_objectivesAddress_calldata());
+		} catch (error) {
+			console.error(error);
+			throw error;
+		}
+	};
+
+	const build_game_token_systems_score_calldata = (tokenId: BigNumberish): DojoCall => {
+		return {
+			contractName: "game_token_systems",
+			entrypoint: "score",
+			calldata: [tokenId],
+		};
+	};
+
+	const game_token_systems_score = async (tokenId: BigNumberish) => {
+		try {
+			return await provider.call("hexed", build_game_token_systems_score_calldata(tokenId));
+		} catch (error) {
+			console.error(error);
+			throw error;
+		}
+	};
+
+	const build_game_token_systems_scoreBatch_calldata = (tokenIds: Array<BigNumberish>): DojoCall => {
+		return {
+			contractName: "game_token_systems",
+			entrypoint: "score_batch",
+			calldata: [tokenIds],
+		};
+	};
+
+	const game_token_systems_scoreBatch = async (tokenIds: Array<BigNumberish>) => {
+		try {
+			return await provider.call("hexed", build_game_token_systems_scoreBatch_calldata(tokenIds));
+		} catch (error) {
+			console.error(error);
+			throw error;
+		}
+	};
+
+	const build_game_token_systems_settingsAddress_calldata = (): DojoCall => {
+		return {
+			contractName: "game_token_systems",
+			entrypoint: "settings_address",
+			calldata: [],
+		};
+	};
+
+	const game_token_systems_settingsAddress = async () => {
+		try {
+			return await provider.call("hexed", build_game_token_systems_settingsAddress_calldata());
+		} catch (error) {
+			console.error(error);
+			throw error;
+		}
+	};
+
+	const build_game_token_systems_supportsInterface_calldata = (interfaceId: BigNumberish): DojoCall => {
+		return {
+			contractName: "game_token_systems",
+			entrypoint: "supports_interface",
+			calldata: [interfaceId],
+		};
+	};
+
+	const game_token_systems_supportsInterface = async (interfaceId: BigNumberish) => {
+		try {
+			return await provider.call("hexed", build_game_token_systems_supportsInterface_calldata(interfaceId));
+		} catch (error) {
+			console.error(error);
+			throw error;
+		}
+	};
+
+	const build_game_token_systems_tokenAddress_calldata = (): DojoCall => {
+		return {
+			contractName: "game_token_systems",
+			entrypoint: "token_address",
+			calldata: [],
+		};
+	};
+
+	const game_token_systems_tokenAddress = async () => {
+		try {
+			return await provider.call("hexed", build_game_token_systems_tokenAddress_calldata());
 		} catch (error) {
 			console.error(error);
 			throw error;
@@ -115,6 +285,28 @@ export function setupWorld(provider: DojoProvider) {
 			buildRegisterScoreCalldata: build_game_systems_registerScore_calldata,
 			spawn: game_systems_spawn,
 			buildSpawnCalldata: build_game_systems_spawn_calldata,
+		},
+		game_token_systems: {
+			gameOver: game_token_systems_gameOver,
+			buildGameOverCalldata: build_game_token_systems_gameOver_calldata,
+			gameOverBatch: game_token_systems_gameOverBatch,
+			buildGameOverBatchCalldata: build_game_token_systems_gameOverBatch_calldata,
+			mintGame: game_token_systems_mintGame,
+			buildMintGameCalldata: build_game_token_systems_mintGame_calldata,
+			mintGameBatch: game_token_systems_mintGameBatch,
+			buildMintGameBatchCalldata: build_game_token_systems_mintGameBatch_calldata,
+			objectivesAddress: game_token_systems_objectivesAddress,
+			buildObjectivesAddressCalldata: build_game_token_systems_objectivesAddress_calldata,
+			score: game_token_systems_score,
+			buildScoreCalldata: build_game_token_systems_score_calldata,
+			scoreBatch: game_token_systems_scoreBatch,
+			buildScoreBatchCalldata: build_game_token_systems_scoreBatch_calldata,
+			settingsAddress: game_token_systems_settingsAddress,
+			buildSettingsAddressCalldata: build_game_token_systems_settingsAddress_calldata,
+			supportsInterface: game_token_systems_supportsInterface,
+			buildSupportsInterfaceCalldata: build_game_token_systems_supportsInterface_calldata,
+			tokenAddress: game_token_systems_tokenAddress,
+			buildTokenAddressCalldata: build_game_token_systems_tokenAddress_calldata,
 		},
 	};
 }
